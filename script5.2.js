4e11da25d62ed0475f95b93359fbd2ef53b7d4aa
Konika13 <konika.agarwal1994@gmail.com>
2017-01-22 00:55:02 +0530

    Add files via upload

---
+++ script5.2.js
@@ -0,0 +1,8 @@
+var obj = {name:'Konika Agarwal' , age:22, DOB:'13/09/1994', place:'Bilaspur'};
+
+console.log("Details are as follows:")
+
+console.log(obj.name)
+console.log(obj.age)
+console.log(obj.DOB)
+console.log(obj.place)
